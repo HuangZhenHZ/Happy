7964fd370e6e23437c46efa1aa3826cb640e7fe4
HuangZhen <1412690197@qq.com>
2024-10-07 22:28:23 +0800

    vec2

--- vec.h
+++ vec.h
@@ -2,7 +2,19 @@
 
 #include <cmath>
 
-template <class T>
+// TODO: Optimize this function
+double NormalizeAngle(double x) {
+  x = std::fmod(x, 2.0 * M_PI);
+  if (x >= M_PI) {
+    return x - 2.0 * M_PI;
+  } else if (x < -M_PI) {
+    return x + 2.0 * M_PI;
+  } else {
+    return x;
+  }
+}
+
+template <typename T>
 struct Vec3 {
   static_assert(std::is_floating_point_v<T>);
   T x = 0.0, y = 0.0, z = 0.0;
@@ -20,7 +32,7 @@ struct Vec3 {
     y *= inv;
     z *= inv;
   }
-  Vec3 Normalized() const {
+  [[nodiscard]] Vec3 Normalized() const {
     const T inv = 1.0 / Length();
     return Vec3(x * inv, y * inv, z * inv);
   }
@@ -83,3 +95,113 @@ using Vec3d = Vec3<double>;
 
 static_assert(sizeof(Vec3f) == sizeof(float) * 3);
 static_assert(sizeof(Vec3d) == sizeof(double) * 3);
+
+
+template <typename T>
+struct Vec2 {
+  static_assert(std::is_floating_point_v<T>);
+  T x = 0.0, y = 0.0;
+  constexpr Vec2() = default;
+  constexpr Vec2(T x, T y) : x(x), y(y) {}
+  static Vec2 FromUnit(T heading) {
+    return Vec2(std::cos(heading), std::sin(heading));
+  }
+  T LengthSqr() const {
+    return x * x + y * y;
+  }
+  T Length() const {
+    return std::sqrt(x * x + y * y);
+  }
+  T Angle() const {
+    return std::atan2(y, x);
+  }
+  void Normalize() {
+    const T inv = 1.0 / Length();
+    x *= inv;
+    y *= inv;
+  }
+  [[nodiscard]] Vec2 Normalized() const {
+    const T inv = 1.0 / Length();
+    return Vec2(x * inv, y * inv);
+  }
+
+  T InnerProd(const Vec2& v) const {
+    return x * v.x + y * v.y;
+  }
+  T CrossProd(const Vec2& v) const {
+    return x * v.y - y * v.x;
+  }
+
+  Vec2 operator+ (const Vec2& v) const {
+    return Vec2(x + v.x, y + v.y);
+  }
+  Vec2 operator- (const Vec2& v) const {
+    return Vec2(x - v.x, y - v.y);
+  }
+  Vec2 operator* (T t) const {
+    return Vec2(x * t, y * t);
+  }
+  Vec2 operator/ (T t) const {
+    return Vec2(x / t, y / t);
+  }
+  friend Vec2 operator* (T t, const Vec2& v) {
+    return Vec2(v.x * t, v.y * t);
+  }
+  Vec2 operator- () const {
+    return Vec2(-x , -y);
+  }
+
+  Vec2& operator+= (const Vec2& v) {
+    x += v.x;
+    y += v.y;
+    return *this;
+  }
+  Vec2& operator-= (const Vec2& v) {
+    x -= v.x;
+    y -= v.y;
+    return *this;
+  }
+  Vec2& operator*= (T t) {
+    x *= t;
+    y *= t;
+    return *this;
+  }
+  Vec2& operator/= (T t) {
+    x /= t;
+    y /= t;
+    return *this;
+  }
+
+  [[nodiscard]] Vec2 RotatedByUnit(double cos_angle, double sin_angle) const {
+    return Vec2(x * cos_angle - y * sin_angle,
+                x * sin_angle + y * cos_angle);
+  }
+  [[nodiscard]] Vec2 Rotated90() const {
+    return Vec2(-y, x);
+  }
+  [[nodiscard]] Vec2 Rotated180() const {
+    return Vec2(-x, -y);
+  }
+  [[nodiscard]] Vec2 Rotated270() const {
+    return Vec2(y, -x);
+  }
+
+  void RotateByUnit(double cos_angle, double sin_angle) {
+    *this = RotatedByUnit(cos_angle, sin_angle);
+  }
+  void Rotate90() {
+    *this = Rotated90();
+  }
+  void Rotate180() {
+    *this = Rotated180();
+  }
+  void Rotate270() {
+    *this = Rotated270();
+  }
+};
+
+using Vec2f = Vec2<float>;
+using Vec2d = Vec2<double>;
+
+static_assert(sizeof(Vec2f) == sizeof(float) * 2);
+static_assert(sizeof(Vec2d) == sizeof(double) * 2);
--- vec_test.cpp
+++ vec_test.cpp
@@ -2,70 +2,60 @@
 
 #include "gtest/gtest.h"
 
+#define EXPECT_VEC3_NEAR(vec3, expect_x, expect_y, expect_z, error) \
+  EXPECT_NEAR(vec3.x, expect_x, error); \
+  EXPECT_NEAR(vec3.y, expect_y, error); \
+  EXPECT_NEAR(vec3.z, expect_z, error);
+
+#define EXPECT_VEC3_EQ(vec3, expect_x, expect_y, expect_z) \
+  EXPECT_EQ(vec3.x, expect_x); \
+  EXPECT_EQ(vec3.y, expect_y); \
+  EXPECT_EQ(vec3.z, expect_z);
+
 TEST(Vec3Test, Add) {
   Vec3f a(1, 2, 3);
   Vec3f b(4, 5, 6);
   Vec3f result = a + b;
-  EXPECT_NEAR(result.x, 5, 1e-9);
-  EXPECT_NEAR(result.y, 7, 1e-9);
-  EXPECT_NEAR(result.z, 9, 1e-9);
+  EXPECT_VEC3_NEAR(result, 5, 7, 9, 1e-9);
   a += b;
-  EXPECT_NEAR(a.x, 5, 1e-9);
-  EXPECT_NEAR(a.y, 7, 1e-9);
-  EXPECT_NEAR(a.z, 9, 1e-9);
+  EXPECT_VEC3_NEAR(a, 5, 7, 9, 1e-9);
   a += a;
-  EXPECT_NEAR(a.x, 10, 1e-9);
-  EXPECT_NEAR(a.y, 14, 1e-9);
-  EXPECT_NEAR(a.z, 18, 1e-9);
+  EXPECT_VEC3_NEAR(a, 10, 14, 18, 1e-9);
 }
 
 TEST(Vec3Test, Sub) {
   Vec3f a(6, 5, 4);
   Vec3f b(1, 2, 3);
   Vec3f result = a - b;
-  EXPECT_NEAR(result.x, 5, 1e-9);
-  EXPECT_NEAR(result.y, 3, 1e-9);
-  EXPECT_NEAR(result.z, 1, 1e-9);
+  EXPECT_VEC3_NEAR(result, 5, 3, 1, 1e-9);
+  Vec3f c = -a;
+  EXPECT_VEC3_NEAR(c, -6, -5, -4, 1e-9);
   a -= b;
-  EXPECT_NEAR(a.x, 5, 1e-9);
-  EXPECT_NEAR(a.y, 3, 1e-9);
-  EXPECT_NEAR(a.z, 1, 1e-9);
+  EXPECT_VEC3_NEAR(a, 5, 3, 1, 1e-9);
   a -= a;
-  EXPECT_EQ(a.x, 0);
-  EXPECT_EQ(a.y, 0);
-  EXPECT_EQ(a.z, 0);
+  EXPECT_VEC3_EQ(a, 0, 0, 0);
 }
 
 TEST(Vec3Test, Mul) {
   Vec3f a(1, 2, 3);
   {
     Vec3f result = a * 1.5;
-    EXPECT_NEAR(result.x, 1.5, 1e-9);
-    EXPECT_NEAR(result.y, 3.0, 1e-9);
-    EXPECT_NEAR(result.z, 4.5, 1e-9);
+    EXPECT_VEC3_NEAR(result, 1.5, 3.0, 4.5, 1e-9);
   }
   {
     Vec3f result = 1.5 * a;
-    EXPECT_NEAR(result.x, 1.5, 1e-9);
-    EXPECT_NEAR(result.y, 3.0, 1e-9);
-    EXPECT_NEAR(result.z, 4.5, 1e-9);
+    EXPECT_VEC3_NEAR(result, 1.5, 3.0, 4.5, 1e-9);
   }
   a *= 1.5;
-  EXPECT_NEAR(a.x, 1.5, 1e-9);
-  EXPECT_NEAR(a.y, 3.0, 1e-9);
-  EXPECT_NEAR(a.z, 4.5, 1e-9);
+  EXPECT_VEC3_NEAR(a, 1.5, 3.0, 4.5, 1e-9);
 }
 
 TEST(Vec3Test, Div) {
   Vec3f a(1, 2, 3);
   Vec3f result = a / 2;
-  EXPECT_NEAR(result.x, 0.5, 1e-9);
-  EXPECT_NEAR(result.y, 1.0, 1e-9);
-  EXPECT_NEAR(result.z, 1.5, 1e-9);
+  EXPECT_VEC3_NEAR(result, 0.5, 1.0, 1.5, 1e-9);
   a /= 2;
-  EXPECT_NEAR(a.x, 0.5, 1e-9);
-  EXPECT_NEAR(a.y, 1.0, 1e-9);
-  EXPECT_NEAR(a.z, 1.5, 1e-9);
+  EXPECT_VEC3_NEAR(a, 0.5, 1.0, 1.5, 1e-9);
 }
 
 TEST(Vec3Test, Length) {
@@ -94,12 +84,8 @@ TEST(Vec3Test, Normalize) {
   EXPECT_NEAR(nb.z, b.z / b.Length(), 1e-6);
   a.Normalize();
   b.Normalize();
-  EXPECT_EQ(a.x, na.x);
-  EXPECT_EQ(a.y, na.y);
-  EXPECT_EQ(a.z, na.z);
-  EXPECT_EQ(b.x, nb.x);
-  EXPECT_EQ(b.y, nb.y);
-  EXPECT_EQ(b.z, nb.z);
+  EXPECT_VEC3_EQ(a, na.x, na.y, na.z);
+  EXPECT_VEC3_EQ(b, nb.x, nb.y, nb.z);
 }
 
 TEST(Vec3Test, InnerProd) {
@@ -115,36 +101,178 @@ TEST(Vec3Test, CrossProd) {
   Vec3f a(2, 0, 0);
   Vec3f b(0, 3, 0);
   Vec3f c(0, 0, 4);
-
   Vec3f ab = a.CrossProd(b);
-  EXPECT_NEAR(ab.x, 0, 1e-9);
-  EXPECT_NEAR(ab.y, 0, 1e-9);
-  EXPECT_NEAR(ab.z, 6, 1e-9);
-
   Vec3f ba = b.CrossProd(a);
-  EXPECT_NEAR(ba.x, 0, 1e-9);
-  EXPECT_NEAR(ba.y, 0, 1e-9);
-  EXPECT_NEAR(ba.z, -6, 1e-9);
-
   Vec3f ca = c.CrossProd(a);
-  EXPECT_NEAR(ca.x, 0, 1e-9);
-  EXPECT_NEAR(ca.y, 8, 1e-9);
-  EXPECT_NEAR(ca.z, 0, 1e-9);
-
   Vec3f ac = a.CrossProd(c);
-  EXPECT_NEAR(ac.x, 0, 1e-9);
-  EXPECT_NEAR(ac.y, -8, 1e-9);
-  EXPECT_NEAR(ac.z, 0, 1e-9);
-
   Vec3f bc = b.CrossProd(c);
-  EXPECT_NEAR(bc.x, 12, 1e-9);
-  EXPECT_NEAR(bc.y, 0, 1e-9);
-  EXPECT_NEAR(bc.z, 0, 1e-9);
-
   Vec3f cb = c.CrossProd(b);
-  EXPECT_NEAR(cb.x, -12, 1e-9);
-  EXPECT_NEAR(cb.y, 0, 1e-9);
-  EXPECT_NEAR(cb.z, 0, 1e-9);
+  EXPECT_VEC3_NEAR(ab, 0, 0, 6, 1e-9);
+  EXPECT_VEC3_NEAR(ba, 0, 0, -6, 1e-9);
+  EXPECT_VEC3_NEAR(ca, 0, 8, 0, 1e-9);
+  EXPECT_VEC3_NEAR(ac, 0, -8, 0, 1e-9);
+  EXPECT_VEC3_NEAR(bc, 12, 0, 0, 1e-9);
+  EXPECT_VEC3_NEAR(cb, -12, 0, 0, 1e-9);
+}
+
+TEST(Vec2Test, Add) {
+  Vec2f a(1, 2);
+  Vec2f b(3, 4);
+  Vec2f result = a + b;
+  EXPECT_NEAR(result.x, 4, 1e-9);
+  EXPECT_NEAR(result.y, 6, 1e-9);
+  a += b;
+  EXPECT_NEAR(a.x, 4, 1e-9);
+  EXPECT_NEAR(a.y, 6, 1e-9);
+  a += a;
+  EXPECT_NEAR(a.x, 8, 1e-9);
+  EXPECT_NEAR(a.y, 12, 1e-9);
+}
+
+TEST(Vec2Test, Sub) {
+  Vec2f a(4, 3);
+  Vec2f b(1, 2);
+  Vec2f result = a - b;
+  EXPECT_NEAR(result.x, 3, 1e-9);
+  EXPECT_NEAR(result.y, 1, 1e-9);
+  Vec2f c = -a;
+  EXPECT_NEAR(c.x, -4, 1e-9);
+  EXPECT_NEAR(c.y, -3, 1e-9);
+  a -= b;
+  EXPECT_NEAR(a.x, 3, 1e-9);
+  EXPECT_NEAR(a.y, 1, 1e-9);
+  a -= a;
+  EXPECT_EQ(a.x, 0);
+  EXPECT_EQ(a.y, 0);
+}
+
+TEST(Vec2Test, Mul) {
+  Vec2f a(1, 2);
+  {
+    Vec2f result = a * 1.5;
+    EXPECT_NEAR(result.x, 1.5, 1e-9);
+    EXPECT_NEAR(result.y, 3.0, 1e-9);
+  }
+  {
+    Vec2f result = 1.5 * a;
+    EXPECT_NEAR(result.x, 1.5, 1e-9);
+    EXPECT_NEAR(result.y, 3.0, 1e-9);
+  }
+  a *= 1.5;
+  EXPECT_NEAR(a.x, 1.5, 1e-9);
+  EXPECT_NEAR(a.y, 3.0, 1e-9);
+}
+
+TEST(Vec2Test, Div) {
+  Vec2f a(1, 2);
+  Vec2f result = a / 2;
+  EXPECT_NEAR(result.x, 0.5, 1e-9);
+  EXPECT_NEAR(result.y, 1.0, 1e-9);
+  a /= 2;
+  EXPECT_NEAR(a.x, 0.5, 1e-9);
+  EXPECT_NEAR(a.y, 1.0, 1e-9);
+}
+
+TEST(Vec2Test, Basic) {
+  const auto test = [](float length, float heading) {
+    Vec2f a = length * Vec2f::FromUnit(heading);
+    EXPECT_EQ(a.x, length * std::cos(heading));
+    EXPECT_EQ(a.y, length * std::sin(heading));
+    EXPECT_NEAR(a.Length(), std::abs(length), 1e-6);
+    EXPECT_NEAR(a.LengthSqr(), length * length, 1e-6);
+    double expected_angle = length > 0 ? NormalizeAngle(heading) :
+        NormalizeAngle(heading + M_PI);
+    EXPECT_NEAR(a.Angle(), expected_angle, 1e-6);
+  };
+  test(1.0, 1.23);
+  test(-1.0, 1.23);
+  test(3.0, 0.6);
+  test(-3.0, 0.6);
+  test(5.5, 123);
+  test(5.5, -123);
+  test(-5.5, 123);
+  test(-5.5, -123);
+}
+
+TEST(Vec2Test, Normalize) {
+  Vec2f a(1, 2);
+  Vec2f b(3, 4);
+  Vec2f na = a.Normalized();
+  Vec2f nb = b.Normalized();
+  EXPECT_NEAR(na.LengthSqr(), 1, 1e-6);
+  EXPECT_NEAR(nb.LengthSqr(), 1, 1e-6);
+  EXPECT_NEAR(na.Length(), 1, 1e-6);
+  EXPECT_NEAR(nb.Length(), 1, 1e-6);
+  EXPECT_NEAR(na.x, a.x / a.Length(), 1e-6);
+  EXPECT_NEAR(na.y, a.y / a.Length(), 1e-6);
+  EXPECT_NEAR(nb.x, b.x / b.Length(), 1e-6);
+  EXPECT_NEAR(nb.y, b.y / b.Length(), 1e-6);
+  a.Normalize();
+  b.Normalize();
+  EXPECT_EQ(a.x, na.x);
+  EXPECT_EQ(a.y, na.y);
+  EXPECT_EQ(b.x, nb.x);
+  EXPECT_EQ(b.y, nb.y);
+}
+
+TEST(Vec2Test, InnerProdAndCrossProd) {
+  Vec2f a(1, 2);
+  Vec2f b(3, 4);
+  EXPECT_NEAR(a.InnerProd(b), 11, 1e-9);
+  EXPECT_NEAR(b.InnerProd(a), 11, 1e-9);
+  EXPECT_EQ(a.InnerProd(a), a.LengthSqr());
+  EXPECT_EQ(b.InnerProd(b), b.LengthSqr());
+  EXPECT_NEAR(a.CrossProd(b), -2, 1e-9);
+  EXPECT_NEAR(b.CrossProd(a), 2, 1e-9);
+  EXPECT_EQ(a.CrossProd(a), 0);
+  EXPECT_EQ(b.CrossProd(b), 0);
+  EXPECT_EQ(a.CrossProd(b), a.Rotated90().InnerProd(b));
+  EXPECT_EQ(a.CrossProd(b), a.InnerProd(b.Rotated270()));
+  EXPECT_EQ(b.CrossProd(a), b.Rotated90().InnerProd(a));
+  EXPECT_EQ(b.CrossProd(a), b.InnerProd(a.Rotated270()));
+}
+
+TEST(Vec2Test, Rotate) {
+  const Vec2f vecs[] = {
+    3.0 * Vec2f::FromUnit(1.23),
+    3.0 * Vec2f::FromUnit(1.23 + M_PI_2),
+    3.0 * Vec2f::FromUnit(1.23 + M_PI),
+    3.0 * Vec2f::FromUnit(1.23 - M_PI_2),
+  };
+  for (int i = 0; i < 4; ++i) {
+    Vec2f rotate90 = vecs[i].Rotated90();
+    EXPECT_NEAR(rotate90.x, vecs[(i + 1) % 4].x, 1e-6);
+    EXPECT_NEAR(rotate90.y, vecs[(i + 1) % 4].y, 1e-6);
+
+    Vec2f rotate180 = vecs[i].Rotated180();
+    EXPECT_NEAR(rotate180.x, vecs[(i + 2) % 4].x, 1e-6);
+    EXPECT_NEAR(rotate180.y, vecs[(i + 2) % 4].y, 1e-6);
+
+    Vec2f rotate270 = vecs[i].Rotated270();
+    EXPECT_NEAR(rotate270.x, vecs[(i + 3) % 4].x, 1e-6);
+    EXPECT_NEAR(rotate270.y, vecs[(i + 3) % 4].y, 1e-6);
+
+    Vec2f a = vecs[i];
+    a.Rotate90();
+    EXPECT_NEAR(a.x, vecs[(i + 1) % 4].x, 1e-6);
+    EXPECT_NEAR(a.y, vecs[(i + 1) % 4].y, 1e-6);
+    a.Rotate180();
+    EXPECT_NEAR(a.x, vecs[(i + 3) % 4].x, 1e-6);
+    EXPECT_NEAR(a.y, vecs[(i + 3) % 4].y, 1e-6);
+    a.Rotate270();
+    EXPECT_NEAR(a.x, vecs[(i + 2) % 4].x, 1e-6);
+    EXPECT_NEAR(a.y, vecs[(i + 2) % 4].y, 1e-6);
+  }
+
+  const Vec2f a = 3.0 * Vec2f::FromUnit(1.23);
+  const Vec2f b = a.RotatedByUnit(std::cos(0.45), std::sin(0.45));
+  const Vec2f c = 3.0 * Vec2f::FromUnit(1.23 + 0.45);
+  EXPECT_NEAR(b.x, c.x, 1e-6);
+  EXPECT_NEAR(b.y, c.y, 1e-6);
+  Vec2f d = a;
+  d.RotateByUnit(std::cos(0.45), std::sin(0.45));
+  EXPECT_NEAR(d.x, c.x, 1e-6);
+  EXPECT_NEAR(d.y, c.y, 1e-6);
 }
 
 int main(int argc, char **argv) {
