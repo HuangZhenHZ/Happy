68de7ae88eaba37153dc20f55334dc4c940bf94a
HuangZhen <1412690197@qq.com>
2025-04-15 09:28:41 +0800

    box2d benchmark

--- eigen_main.cpp
+++ eigen_main.cpp
@@ -1,4 +1,5 @@
 #include "Eigen/Core"
+#include "math/vec.h"
 // #include "fmt/core.h"
 #include <cstdio>
 
@@ -12,6 +13,28 @@ int main() {
   for (int i = 0; i < 10; ++i) {
     // fmt::print("{}\n", c(i));
     printf("%lf\n", c(i));
+  }
+
+  std::vector<Vec2d> points;
+  for (int i = 0; i < 10; ++i) {
+    points.emplace_back(i, i + 1);
+  }
+  for (const Vec2d& point : points) {
+    printf("%lf %lf\n", point.x, point.y);
+  }
+  putchar('\n');
+  Eigen::Map<Eigen::Matrix2Xd> mp(&points[0].x, 2, points.size());
+  Eigen::Matrix2d mat;
+  mat << 0, 1, 1, 0;
+  Eigen::Vector2d vec(2, 4);
+  /*
+  mp.colwise() -= vec;
+  mp.applyOnTheLeft(mat);
+  mp.colwise() += vec;
+  */
+  mp = (mat * (mp.colwise() - vec)).colwise() + vec;
+  for (const Vec2d& point : points) {
+    printf("%lf %lf\n", point.x, point.y);
   }
   return 0;
 }
---
+++ math/box2d_benchmark.mk
@@ -0,0 +1,4 @@
+SRCS = math/box2d_benchmark.cpp src/benchmark.cpp
+TARGET = math/box2d_benchmark
+LDFLAGS = -lshlwapi
+include template.mk
